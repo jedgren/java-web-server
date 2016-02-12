83b49e034d60a12d33885fc9f287408d49c7e458
Jan Edgren <jan.edgren@gmp-systems.com>
2016-02-12 21:29:59 +0100

    Basic HttpServer configuration

--- src/main/java/se/enelefant/JavaWebServer.java
+++ src/main/java/se/enelefant/JavaWebServer.java
@@ -1,9 +1,15 @@
 package se.enelefant;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+
 public class JavaWebServer {
 
-    public static void main(String[] args) {
-        // TODO: Add web server setup
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.setExecutor(null);
+        server.start();
     }
 
 }
