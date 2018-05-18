944066fcd67a14c7ae1044f925c7b89c969148d3
Jan Edgren <jan@edgren.nu>
2018-05-18 03:06:55 +0200

    Fixes endpoint registration and adds registration of filters

--- src/main/java/se/enelefant/service/HandlerEndpointRegistryService.java
+++ src/main/java/se/enelefant/service/HandlerEndpointRegistryService.java
@@ -6,12 +6,12 @@ import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import se.enelefant.handler.HandlerEndpoint;
-import se.enelefant.handler.filter.HandlerFilter;
 import se.enelefant.helper.LoggerHelper;
 
 import java.io.File;
 import java.lang.reflect.Method;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,13 +28,20 @@ public class HandlerEndpointRegistryService {
     public void registerEndpoints(HttpServer server) throws Exception {
         logger.info("Registering endpoints...");
         Map<String, Class<HttpHandler>> endpoints = findEndpoints(new HashMap<>(), "se.enelefant.handler");
+
+        List<Filter> handlerFilters = findHandlerFilters(new ArrayList<>(), "se.enelefant.handler.filter");
+
         for (String path : endpoints.keySet()) {
             Class<HttpHandler> httpHandlerClass = endpoints.get(path);
+            HttpContext context = server.createContext(path, httpHandlerClass.newInstance());
+            for (Filter filter : handlerFilters) {
+                context.getFilters().add(filter);
+            }
             logger.info("Registering " + httpHandlerClass.getSimpleName() + " at endpoint '" + path + "'");
         }
     }
 
-    private List<Class<Filter>> getHandlerFilters(List<Class<Filter>> filters, String scannedPackage) {
+    private List<Filter> findHandlerFilters(List<Class<Filter>> filterClasses, String scannedPackage) throws IllegalAccessException, InstantiationException {
         String scannedPath = scannedPackage.replace(PKG_SEPARATOR, DIR_SEPARATOR);
         URL scannedUrl = ClassLoader.getSystemClassLoader().getResource(scannedPath);
         if (scannedUrl == null) {
@@ -42,7 +49,13 @@ public class HandlerEndpointRegistryService {
         }
         File scannedDir = new File(scannedUrl.getFile());
         for (File file : scannedDir.listFiles()) {
-            findFilters(filters, file, scannedPackage);
+            findFilters(filterClasses, file, scannedPackage);
+        }
+        List<Filter> filters = new ArrayList<>();
+        for (Class<Filter> filterClass : filterClasses) {
+            Filter filter = filterClass.newInstance();
+            logger.info("Registering filter " + filterClass.getSimpleName());
+            filters.add(filter);
         }
         return filters;
     }
@@ -58,13 +71,8 @@ public class HandlerEndpointRegistryService {
             String className = resource.substring(0, endIndex);
             try {
                 Class<?> potentialClass = Class.forName(className);
-                if (HttpHandler.class.isAssignableFrom(potentialClass)) {
-                    for (Method method : potentialClass.getMethods()) {
-                        HandlerFilter annotation = method.getAnnotation(HandlerFilter.class);
-                        if (annotation != null) {
-                            filters.add((Class<Filter>) potentialClass);
-                        }
-                    }
+                if (Filter.class.isAssignableFrom(potentialClass)) {
+                    filters.add((Class<Filter>) potentialClass);
                 }
             } catch (ClassNotFoundException ignore) {
             }
